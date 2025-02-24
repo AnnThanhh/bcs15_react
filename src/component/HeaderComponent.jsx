e18bf636b9317a2151c4f6a4cd192b769ccc272c
AnnThanhhh <clear>
2025-02-24 09:28:19 +0700

    first commit

---
+++ src/component/HeaderComponent.jsx
@@ -0,0 +1,5 @@
+const HeaderComponent = () => {
+  return <div>Header</div>;
+};
+
+export default HeaderComponent;
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import HeaderComponent from "./component/HeaderComponent";
+
+createRoot(document.getElementById("root")).render(
+<HeaderComponent />
+);
